efc6b80b31707d5390e49b17013345e4a416a8c8
Nikita Khlebushkin <nikita.khlebushkin@gmail.com>
2021-05-19 00:08:16 +0300

    Add navigationController

--- app/src/main/java/com/xlebnick/cameraandgallery/MainActivity.kt
+++ app/src/main/java/com/xlebnick/cameraandgallery/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.xlebnick.cameraandgallery
 
 import android.os.Bundle
-import com.xlebnick.cameraandgallery.ui.main.MainFragment
+import androidx.navigation.findNavController
+import com.xlebnick.cameraandgallery.utils.ActivityNavControllerHelper
 import dagger.android.support.DaggerAppCompatActivity
+import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity() {
+
+    @Inject
+    lateinit var navControllerHelper: ActivityNavControllerHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment())
-                .commitNow()
-        }
+
+        navControllerHelper.setNavController(findNavController(R.id.navHostFragment))
     }
 }
